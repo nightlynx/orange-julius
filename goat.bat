3f048713bf4ba2457dcba7029c642977195a663b
nightlynx <james.jacobs@gmail.com>
2023-05-24 13:34:18 -0400

    Add files via upload

---
+++ goat.bat
@@ -0,0 +1,26 @@
+@echo off
+
+echo " /     \             \            /    \" > C:\Windows\Temp\goat.txt       
+echo "|       |             \          |      |" >> C:\Windows\Temp\goat.txt      
+echo "|       `.             |         |       :" >> C:\Windows\Temp\goat.txt     
+echo "`        |             |        \|       |" >> C:\Windows\Temp\goat.txt     
+echo " \       | /       /  \\\   --__ \\       :" >> C:\Windows\Temp\goat.txt    
+echo "  \      \/   _--~~          ~--__| \     |" >> C:\Windows\Temp\goat.txt    
+echo "   \      \_-~                    ~-_\    |" >> C:\Windows\Temp\goat.txt    
+echo "    \_     \        _.--------.______\|   |" >> C:\Windows\Temp\goat.txt    
+echo "      \     \______// _ ___ _ (_(__>  \   |" >> C:\Windows\Temp\goat.txt    
+echo "       \   .  C ___)  ______ (_(____>  |  /" >> C:\Windows\Temp\goat.txt    
+echo "       /\ |   C ____)/      \ (_____>  |_/" >> C:\Windows\Temp\goat.txt     
+echo "      / /\|   C_____)       |  (___>   /  \" >> C:\Windows\Temp\goat.txt    
+echo "     |   (   _C_____)\______/  // _/ /     \" >> C:\Windows\Temp\goat.txt   
+echo "     |    \  |__   \\_________// (__/       |" >> C:\Windows\Temp\goat.txt  
+echo "    | \    \____)   `----   --'             |" >> C:\Windows\Temp\goat.txt  
+echo "    |  \_          ___\       /_          _/ |" >> C:\Windows\Temp\goat.txt 
+echo "   |              /    |     |  \            |" >> C:\Windows\Temp\goat.txt 
+echo "   |             |    /       \  \           |" >> C:\Windows\Temp\goat.txt 
+echo "   |          / /    |         |  \           |" >> C:\Windows\Temp\goat.txt
+echo "   |         / /      \__/\___/    |          |" >> C:\Windows\Temp\goat.txt
+echo "  |           /        |    |       |         |" >> C:\Windows\Temp\goat.txt
+echo "  |          |         |    |       |         |" >> C:\Windows\Temp\goat.txt
+type C:\Windows\Temp\goat.txt | msg *
+del C:\Windows\Temp\goat.txt
